a01246f43aaad8d8446304bada1724b00e304f51
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-04-01 21:31:01 +0300

    fetch API implementation

--- js/main.js
+++ js/main.js
@@ -162,8 +162,74 @@
 
 
 
+// console.log("Fetch Api");
+// async function getUsers() {
+//     try{
+//         const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//         if (!response.ok){
+//             throw new Error(`HTTP ошибка статус${response.status}` );
+//         }
+//         const users = await response.json();
+
+//         console.log("первые три пользователя");
+//         users.slice(0, 3).forEach((user) => {
+//             console.log(`- ${user.name} (${user.email})`);
+//         });
+//     }catch(error){
+//         console.log("Ошибка при загрузке ", error.message);
+//     }
+// }
+
+// getUsers();
 
 
+
+// async function getUserById(id) {
+//     try{
+//         const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+//         const user = await response.json();
+
+//         console.log(`Пользователь #${id}`);
+//         console.log(`Имя ${user.name}`);
+//         console.log(`Город ${user.address.city}`);
+//         console.log(`Компании ${user.company.name}`);
+
+//     }catch(error){
+//         console.log("Ошибка", error.message);
+//     }
+    
+// }
+
+// getUserById(1);
+
+
+
+
+// async function createPost() {
+//   try {
+//     const newPost = {
+//       title: "Моя первая запись",
+//       body: "Это содержание моей первой записи в блоге",
+//       userId: 1,
+//     };
+//     const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+//       method: "POST", 
+//       headers: {
+//         "Content-Type": "application/json", 
+//       },
+//       body: JSON.stringify(newPost), 
+//     });
+    
+//     const createdPost = await response.json();
+//     console.log("Создана новая запись:");
+//     console.log(`ID: ${createdPost.id}`);
+//     console.log(createdPost.title); 
+//   } catch (error) {
+//     console.log("Ошибка при создании записи:", error.message);
+//   }
+// }
+
+// createPost();
 
 
 
